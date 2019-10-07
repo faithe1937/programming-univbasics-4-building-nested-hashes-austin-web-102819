8778a6de0ac9c7e72ea7271f5a4ef36d3b4549a0
Faith Janicek <faithe1937@gmail.com>
2019-10-07 16:14:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,9 @@ def monopoly_with_third_tier
     },
     :names => {
       :reading => {},
-      :pennsylvania_railroad => {}
-      :b_and_o_railroad
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}.
     }
   }
 }
