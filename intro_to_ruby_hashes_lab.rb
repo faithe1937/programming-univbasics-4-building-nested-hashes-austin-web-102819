cbe2b86d9d52b639095910f162dc19a0e792f368
Faith Janicek <faithe1937@gmail.com>
2019-10-07 16:25:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,7 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200,
     },
     :names => {
-      :reading_railroad=> {"mortage_value" => "$100"},
+      :reading_railroad => {"mortage_value" => "$100"},
       :pennsylvania_railroad => {"mortage_value" => "$200"},
       :b_and_o_railroad => {"mortage_value" => "$400"},
       :shortline_railroad => {"mortage_value" => "$800"}
